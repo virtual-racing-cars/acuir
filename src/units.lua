ff87306f3a0da2d22d3372f1a148ff68929043f7
William Gawlik <william.gawlik@outlook.com>
2025-06-14 22:20:55 -0500

    Fixed using imperial units when metric should be used

--- src/units.lua
+++ src/units.lua
@@ -7,8 +7,8 @@ function units:speed(speedKmh)
         return uis.useImperialUnits and speed * 0.621371 or speed
 end
 
-function units:mass(massKg) return massKg * 2.20462 end
+function units:mass(massKg) return uis.useImperialUnits and massKg * 2.20462 or massKg end
 
-function units:temperature(temperatureC) return temperatureC * (9 / 5) + 32 end
+function units:temperature(temperatureC) return uis.useImperialUnits and temperatureC * (9 / 5) + 32 or temperatureC end
 
 return units
